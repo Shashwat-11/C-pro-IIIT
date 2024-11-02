ddfdc1d77c019acf3643119043b07381aaa14b4b
Shashwat-11 <shashwat11mukadam@gmail.com>
2024-11-03 02:38:34 +0530

    Delete Assinments directory

--- Assinments/a5/a5-handout/p2/main.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
-struct node {
-    int val;
-    struct node *next;
-};
-struct node * sortLL(int len, struct node * head);
--- Assinments/a5/a5-handout/p3/main.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
-struct node {
-    char name[11];
-    struct node *next;
-};
-struct node * reverseingroups(struct node * head);
